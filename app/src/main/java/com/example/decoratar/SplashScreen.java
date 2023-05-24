37e71b73a72dd83f9ab92a031ccf70311add70cb
[Khan Mustufa] <[vu4s2021038@pvppcoe.ac.in]>
2023-05-24 19:52:54 +0530

    sample commit

---
+++ app/src/main/java/com/example/decoratar/SplashScreen.java
@@ -0,0 +1,49 @@
+package com.example.decoratar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.VideoView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        VideoView videoView = findViewById(R.id.logo_anim);
+        Uri video = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.logo);
+        videoView.setVideoURI(video);
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                startNextActivity();
+            }
+        });
+
+        videoView.start();
+    }
+    private void startNextActivity(){
+        if (isFinishing())
+            return;
+        startActivity(new Intent(this, MainActivity2.class));
+        finish();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS;
+        }
+    }
+}
